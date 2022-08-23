1f82ecccf1bfbe32e591c95ab9f90fa9f7c819e9
Azathoth <vladislavutkin92@gmail.com>
2022-08-23 06:33:03 +0300

    update Card component.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './components/card/Card';
 function App() {
   return (
     <div class="container">
-      <Card>
+      <Card src={"https://img3.akspic.ru/previews/8/3/3/8/6/168338/168338-nyujork-ulice_nyu_jorka-ulica-manhetten-zdanie-500x.jpg"}>
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel cupiditate perspiciatis reiciendis unde, obcaecati porro quae, aspernatur dolorum eum eveniet ex vitae culpa dolores dignissimos exercitationem praesentium. In, corporis esse.</p>
       </Card>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Card(props) {
+function Card({children, src}) {
   return (
     <div className="card w-25">
-      <img src="https://img3.akspic.ru/previews/8/3/3/8/6/168338/168338-nyujork-ulice_nyu_jorka-ulica-manhetten-zdanie-500x.jpg" className="card-img-top" alt="..." />
+      <img src={src} className="card-img-top" alt="..." />
         <div className="card-body">
-          {props.children}
+          {children}
           <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
     </div>
